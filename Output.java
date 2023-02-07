2a2852d62bf0408cf131a503bf4c7d7dc9620802
takealook <takealook97@naver.com>
2023-02-07 12:34:53 +0900

    출력 수정

--- Output.java
+++ Output.java
@@ -2,7 +2,8 @@ public class Output {
     void printInit() {
         System.out.println("프로세스 시간 = A : 3초 | B : 5초 | C : 7초 | D : 10초 | E : 15초 | F : 21초");
         System.out.println("이 프로그램은 6개의 프로세스(A, B, C, D, E, F) 중 랜덤으로");
-        System.out.println("프로세스A(3초), 프로세스B(5초),프로세스C(7초) 3개를 선택하고 라운드로빈 방식으로 실행합니다." + "\n");
+        System.out.println("프로세스A(3초), 프로세스B(5초),프로세스C(7초) 3개를 선택하고 라운드로빈 방식으로 실행합니다.");
+        System.out.println("-----------------------------------------------------------------------");
     }
 
     void printReady() {
@@ -31,7 +32,7 @@ public class Output {
     void printEnd() {
         //평균 대기시간 = (대기시간 총합) / 3 = (결과)sec
         //평균 반환시간 = (완료시간+대기시간 총합) / 3 = (결과)sec
-        System.out.println();
+        System.out.println("-----------------------------------------------------------------------");
         System.out.println("라운드로빈 방식 스케줄링이 종료되었습니다.");
         System.out.print("평균 대기시간 = (" + Processing.left[0] + " + " + Processing.left[1] + " + " + Processing.left[2] + ") / 3 = ");
         System.out.println(String.format("%.1f", (double) (Processing.left[0] + Processing.left[1] + Processing.left[2]) / 3) + "sec");
